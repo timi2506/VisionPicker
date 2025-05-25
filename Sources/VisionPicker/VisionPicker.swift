fbc8579aaf94ce4fb60d66f303228efb9a8f68a8
timi2506 <172171055+timi2506@users.noreply.github.com>
2025-05-25 18:09:58 +0200

    A

--- Sources/VisionPicker/VisionPicker.swift
+++ Sources/VisionPicker/VisionPicker.swift
@@ -11,8 +11,13 @@ import SwiftUI
 
 public struct PickerItem<ValueType: Hashable, LabelView: View>: Identifiable {
     public let id: UUID = UUID()
-    let value: ValueType
-    let label: () -> LabelView
+    public let value: ValueType
+    public let label: () -> LabelView
+
+    public init(value: ValueType, label: @escaping () -> LabelView) {
+        self.value = value
+        self.label = label
+    }
 }
 
 // MARK: - Result Builder
@@ -24,12 +29,16 @@ public struct PickerItemsBuilder {
     }
 }
 
-public struct VisionPicker<ValueType: Hashable, LabelView: View>: View {
-    @Binding var selection: ValueType
-    let items: [PickerItem<ValueType, LabelView>]
-    var backgroundMaterial: Material = .regularMaterial
+// MARK: - VisionPicker
 
-    init(
+public struct VisionPicker<ValueType: Hashable, LabelView: View>: View {
+    @Binding public var selection: ValueType
+    public let items: [PickerItem<ValueType, LabelView>]
+    public var backgroundMaterial: Material = .regularMaterial
+
+    @Namespace private var namespace
+
+    public init(
         selection: Binding<ValueType>,
         backgroundMaterial: Material? = nil,
         @PickerItemsBuilder items: () -> [PickerItem<ValueType, LabelView>]
@@ -38,24 +47,24 @@ public struct VisionPicker<ValueType: Hashable, LabelView: View>: View {
         self.items = items()
         self.backgroundMaterial = backgroundMaterial ?? .regularMaterial
     }
-    @Namespace private var namespace
+
     public var body: some View {
         HStack(spacing: 5) {
             ForEach(items) { item in
                 item.label()
-                    .onTapGesture{
+                    .onTapGesture {
                         withAnimation() {
                             selection = item.value
                         }
                     }
-                .padding(5)
-                .background {
-                    if selection == item.value {
-                        Capsule()
-                            .foregroundStyle(.gray.opacity(0.25))
-                            .matchedGeometryEffect(id: "PickerShape", in: namespace)
+                    .padding(5)
+                    .background {
+                        if selection == item.value {
+                            Capsule()
+                                .foregroundStyle(.gray.opacity(0.25))
+                                .matchedGeometryEffect(id: "PickerShape", in: namespace)
+                        }
                     }
-                }
             }
         }
         .padding(5)
